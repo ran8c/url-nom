5f5e5fc37b068d4d8496b793d400d3c65f79a9ca
ran8c <ran8c@icloud.com>
2025-01-10 17:20:53 -0800

    Add UrlSearchError skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,63 @@
 
 // TODO: add documentation
 
+pub enum UrlSearchError {}
 pub enum UrlError {}
 
 // https://url.spec.whatwg.org/#api
 
+// https://url.spec.whatwg.org/#interface-urlsearchparams
+pub struct UrlSearchParams {
+    params: Vec<String>,
+}
+
+impl UrlSearchParams {
+    pub fn new(params: &[&str]) -> Result<Self, UrlSearchError> {
+        Ok(todo!())
+    }
+
+    pub fn size() -> u32 {
+        todo!()
+    }
+
+    pub fn append(name: &str, value: &str) {
+        todo!()
+    }
+
+    pub fn delete(name: &str, value: Option<&str>) {
+        todo!()
+    }
+
+    pub fn get(name: &str) -> Option<String> {
+        todo!()
+    }
+
+    pub fn get_all(name: &str) -> Option<Vec<String>> {
+        todo!()
+    }
+
+    pub fn has(name: &str) -> bool {
+        todo!()
+    }
+
+    pub fn set(name: &str, value: &str) {
+        todo!()
+    }
+
+    pub fn stringify() -> String {
+        todo!()
+    }
+}
+
+impl Iterator for UrlSearchParams {
+    type Item = UrlSearchParams;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+// https://url.spec.whatwg.org/#url-class
 pub struct Url {
     protocol: String,
     username: String,
